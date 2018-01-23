494be9807a0d034226177433a244ff0428ee3bdd
GravityMsc <gravity.msc@hotmail.com>
2018-01-23 11:16:34 +0800

    custom Fetch for status code and network error

---
+++ src/components/Common/myFetch.js
@@ -0,0 +1,15 @@
+const myFetch = async (url, options, transform = res => (res.json())) => {
+  let result = '';
+  try {
+    const response = await fetch(url, options);
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    } else {
+      result = await transform(response);
+    }
+  } catch (error) {
+    console.error(error);
+  }
+  return result;
+};
+export default myFetch;
