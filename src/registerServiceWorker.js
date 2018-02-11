95d7c8c2cc43de2f0177d8b05b58a6540ca2dc71
GravityMsc <gravity.msc@hotmail.com>
2018-02-11 16:56:47 +0800

    promise PUSH after Service Worker active

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -3,12 +3,14 @@ const register = () => {
     navigator.serviceWorker.register('/service-worker.js').then((registration) => {
       // 注册成功
       console.log('ServiceWorker registration successful with scope: ', registration.scope);
-      registration.pushManager.subscribe({
-        userVisibleOnly: true,
-      }).then((pushSubscription) => {
-        console.log('ServiceWorker pushSubscription successful: ', pushSubscription);
-      }, (err) => {
-        console.log('ServiceWorker pushSubscription failed: ', err);
+      navigator.serviceWorker.ready.then((ReadyRegistration) => {
+        ReadyRegistration.pushManager.subscribe({
+          userVisibleOnly: true,
+        }).then((pushSubscription) => {
+          console.log('ServiceWorker pushSubscription successful: ', pushSubscription);
+        }, (err) => {
+          console.log('ServiceWorker pushSubscription failed: ', err);
+        });
       });
     }).catch((err) => {
       // 注册失败
