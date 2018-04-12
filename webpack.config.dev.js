283f4891a5edb4fdfd36246e4160db2cc0478eed
GravityMsc <gravity.msc@hotmail.com>
2018-04-12 10:09:38 +0800

    less javascriptEnabled

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -3,100 +3,105 @@ const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: {
-        index: [
-            'react-hot-loader/patch', // activate HMR for React
-            './src/index.jsx', // some entry files
-        ],
-        vendors: ['react', 'prop-types', 'react-dom', 'react-router-dom', 'react-redux', 'redux', 'redux-thunk', 'react-hot-loader'], // all of main libs in this project
-    },
-    output: {
-        path: __dirname + '/dist',
-        filename: '[name]__[chunkhash:8].js',
-        chunkFilename: "[name]__[chunkhash:5]_chunk.js", // used by code splitting
-        publicPath: '/',
-    },
-    module: {
-        rules: [{
-            test: /\.jsx?$/,
-            exclude: /node_modules/,
-            loader: 'babel-loader',
-        }, {
-            test: /\.css$/,
-            use: ['style-loader', 'css-loader', 'postcss-loader'],
-        }, {
-            test: /\.less$/,
-            use: ['style-loader', 'css-loader', 'postcss-loader', 'less-loader'],
-        }, {
-            test: /\.(png|jpe?g|gif)(\?.+)?$/,
-            use: [{
-                loader: 'url-loader',
-                options: {
-                    limit: 10000,
-                    fallback: 'file-loader',
-                    name: '[name].[hash:12].[ext]',
-                    outputPath: './images',
-                }
-            }],
-        }, {
-            test: /\.(ttf|eot|svg|woff|woff2)(\?.+)?$/,
-            use: [{
-                loader: 'file-loader',
-                options: {
-                    name: '[name].[hash:12].[ext]',
-                    outputPath: './fonts',
-                }
-            }],
-        }],
-    },
-    devServer: {
-        contentBase: './src',
-        publicPath: '/',
-        inline: true,
-        hot: true,
-        hotOnly: true,
-        host: '0.0.0.0',
-        port: 8088,
-        disableHostCheck: true,
-        historyApiFallback: true,
-        // proxy: [{
-        //     context: [
-        //         '/api',
-        //     ],
-        //     target: 'http(s)://host:port',
-        //     changeOrigin: true,
-        //     secure: false,
-        // }], // severd by 'http-proxy-middleware', send API requests on the same domain
-    },
-    plugins: [
-        new webpack.optimize.ModuleConcatenationPlugin(),
-        new webpack.optimize.CommonsChunkPlugin({
-            name: 'vendors',
-            filename: 'vendors.js',
-            minChunks: Infinity,
-        }),
-        new webpack.optimize.CommonsChunkPlugin({
-            async: 'commonLazy',
-            children: true,
-            minChunks: 3,
-        }),
-        new webpack.HotModuleReplacementPlugin(),
-        new webpack.NamedModulesPlugin(),
-        new HtmlWebpackPlugin({
-            template: './src/index_tpl.html',
-            filename: 'index.html',
-            favicon: './src/images/favicon.png',
-            inject: true,
-            hash: true,
-        }),
-        new webpack.DefinePlugin({
-            'process.env': {
-                'NODE_ENV': JSON.stringify(process.env.NODE_ENV),
-            },
-        }),
+  entry: {
+    index: [
+      'react-hot-loader/patch', // activate HMR for React
+      './src/index.jsx', // some entry files
     ],
-    resolve: {
-        extensions: ['.jsx', '.js']
-    },
-    devtool: 'source-map',
+    vendors: ['react', 'prop-types', 'react-dom', 'react-router-dom', 'react-redux', 'redux', 'redux-thunk', 'react-hot-loader'], // all of main libs in this project
+  },
+  output: {
+    path: __dirname + '/dist',
+    filename: '[name]__[chunkhash:8].js',
+    chunkFilename: "[name]__[chunkhash:5]_chunk.js", // used by code splitting
+    publicPath: '/',
+  },
+  module: {
+    rules: [{
+      test: /\.jsx?$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader',
+    }, {
+      test: /\.css$/,
+      use: ['style-loader', 'css-loader', 'postcss-loader'],
+    }, {
+      test: /\.less$/,
+      use: ['style-loader', 'css-loader', 'postcss-loader', {
+        loader: 'less-loader',
+        options: {
+          javascriptEnabled: true
+        }
+      }],
+    }, {
+      test: /\.(png|jpe?g|gif)(\?.+)?$/,
+      use: [{
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          fallback: 'file-loader',
+          name: '[name].[hash:12].[ext]',
+          outputPath: './images',
+        }
+      }],
+    }, {
+      test: /\.(ttf|eot|svg|woff|woff2)(\?.+)?$/,
+      use: [{
+        loader: 'file-loader',
+        options: {
+          name: '[name].[hash:12].[ext]',
+          outputPath: './fonts',
+        }
+      }],
+    }],
+  },
+  devServer: {
+    contentBase: './src',
+    publicPath: '/',
+    inline: true,
+    hot: true,
+    hotOnly: true,
+    host: '0.0.0.0',
+    port: 8088,
+    disableHostCheck: true,
+    historyApiFallback: true,
+    // proxy: [{
+    //     context: [
+    //         '/api',
+    //     ],
+    //     target: 'http(s)://host:port',
+    //     changeOrigin: true,
+    //     secure: false,
+    // }], // severd by 'http-proxy-middleware', send API requests on the same domain
+  },
+  plugins: [
+    new webpack.optimize.ModuleConcatenationPlugin(),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendors',
+      filename: 'vendors.js',
+      minChunks: Infinity,
+    }),
+    new webpack.optimize.CommonsChunkPlugin({
+      async: 'commonLazy',
+      children: true,
+      minChunks: 3,
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NamedModulesPlugin(),
+    new HtmlWebpackPlugin({
+      template: './src/index_tpl.html',
+      filename: 'index.html',
+      favicon: './src/images/favicon.png',
+      inject: true,
+      hash: true,
+    }),
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+      },
+    }),
+  ],
+  resolve: {
+    extensions: ['.jsx', '.js']
+  },
+  devtool: 'source-map',
 };
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -7,96 +7,101 @@ const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPl
 const extractLESS = new ExtractTextPlugin('css/[name]__[contenthash].css'); // extract style files to reduce the first load time
 
 module.exports = {
-    entry: {
-        index: [
-            './src/index.jsx',
-        ],
-        vendors: ['react', 'prop-types', 'react-dom', 'react-router-dom', 'react-redux', 'redux', 'redux-thunk'],
-    },
-    output: {
-        path: __dirname + '/dist',
-        filename: 'js/[name]__[chunkhash:8].js',
-        chunkFilename: "js/[name]__[chunkhash:5]_chunk.js",
-        publicPath: '',
-    },
-    module: {
-        rules: [{
-            test: /\.jsx?$/,
-            exclude: /node_modules/,
-            loader: 'babel-loader',
-        }, {
-            test: /\.css$/,
-            use: ['style-loader', 'css-loader', 'postcss-loader'],
-        }, {
-            test: /\.less$/,
-            use: extractLESS.extract({
-                fallback: 'style-loader',
-                use: [
-                    'css-loader',
-                    'postcss-loader',
-                    'less-loader',
-                ],
-                publicPath: '../', // 默认生成路径为style/xxx，所以为了正确匹配到url()内image路径，需要到上级目录
-            }),
-        }, {
-            test: /\.(png|jpe?g|gif)(\?.+)?$/,
-            use: [{
-                loader: 'url-loader',
-                options: {
-                    limit: 10000,
-                    fallback: 'file-loader',
-                    name: '[name].[hash:12].[ext]',
-                    outputPath: './images',
-                }
-            }],
-        }, {
-            test: /\.(ttf|eot|svg|woff|woff2)(\?.+)?$/,
-            use: [{
-                loader: 'file-loader',
-                options: {
-                    name: '[name].[hash:12].[ext]',
-                    outputPath: './fonts',
-                }
-            }],
-        }],
-    },
-    plugins: [
-        new BundleAnalyzerPlugin(),
-        new CopyWebpackPlugin([{
-            from: './src/PWA',
-            to: 'PWA'
-        }, './src/service-worker.js',
-        ]),
-        extractLESS,
-        new webpack.optimize.ModuleConcatenationPlugin(),
-        new webpack.optimize.CommonsChunkPlugin({
-            name: 'vendors',
-            filename: 'js/vendors.js',
-            minChunks: Infinity,
-        }),
-        new webpack.optimize.CommonsChunkPlugin({
-            async: 'commonLazy',
-            children: true,
-            minChunks: 3,
-        }),
-        new webpack.NamedModulesPlugin(),
-        new HtmlWebpackPlugin({
-            template: './src/index_tpl.html',
-            filename: 'index.html',
-            favicon: './src/images/favicon.png',
-            inject: true,
-        }),
-        new webpack.DefinePlugin({
-            'process.env': {
-                'NODE_ENV': JSON.stringify(process.env.NODE_ENV),
-            },
-        }),
-        new webpack.optimize.UglifyJsPlugin({
-            sourceMap: true
-        }),
+  entry: {
+    index: [
+      './src/index.jsx',
     ],
-    resolve: {
-        extensions: ['.jsx', '.js']
-    },
-    devtool: 'source-map',
+    vendors: ['react', 'prop-types', 'react-dom', 'react-router-dom', 'react-redux', 'redux', 'redux-thunk'],
+  },
+  output: {
+    path: __dirname + '/dist',
+    filename: 'js/[name]__[chunkhash:8].js',
+    chunkFilename: "js/[name]__[chunkhash:5]_chunk.js",
+    publicPath: '',
+  },
+  module: {
+    rules: [{
+      test: /\.jsx?$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader',
+    }, {
+      test: /\.css$/,
+      use: ['style-loader', 'css-loader', 'postcss-loader'],
+    }, {
+      test: /\.less$/,
+      use: extractLESS.extract({
+        fallback: 'style-loader',
+        use: [
+          'css-loader',
+          'postcss-loader',
+          {
+            loader: 'less-loader',
+            options: {
+              javascriptEnabled: true
+            }
+          }
+        ],
+        publicPath: '../', // 默认生成路径为style/xxx，所以为了正确匹配到url()内image路径，需要到上级目录
+      }),
+    }, {
+      test: /\.(png|jpe?g|gif)(\?.+)?$/,
+      use: [{
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          fallback: 'file-loader',
+          name: '[name].[hash:12].[ext]',
+          outputPath: './images',
+        }
+      }],
+    }, {
+      test: /\.(ttf|eot|svg|woff|woff2)(\?.+)?$/,
+      use: [{
+        loader: 'file-loader',
+        options: {
+          name: '[name].[hash:12].[ext]',
+          outputPath: './fonts',
+        }
+      }],
+    }],
+  },
+  plugins: [
+    new BundleAnalyzerPlugin(),
+    new CopyWebpackPlugin([{
+      from: './src/PWA',
+      to: 'PWA'
+    }, './src/service-worker.js',
+    ]),
+    extractLESS,
+    new webpack.optimize.ModuleConcatenationPlugin(),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendors',
+      filename: 'js/vendors.js',
+      minChunks: Infinity,
+    }),
+    new webpack.optimize.CommonsChunkPlugin({
+      async: 'commonLazy',
+      children: true,
+      minChunks: 3,
+    }),
+    new webpack.NamedModulesPlugin(),
+    new HtmlWebpackPlugin({
+      template: './src/index_tpl.html',
+      filename: 'index.html',
+      favicon: './src/images/favicon.png',
+      inject: true,
+    }),
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+      },
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      sourceMap: true
+    }),
+  ],
+  resolve: {
+    extensions: ['.jsx', '.js']
+  },
+  devtool: 'source-map',
 };
