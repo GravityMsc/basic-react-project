5d4c795bcb474692cd5f97a9d6cc4d2f6b63c3cd
GravityMsc <gravity.msc@hotmail.com>
2018-06-26 11:04:45 +0800

    Bugfix: undefined params

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,4 +1,6 @@
 /* eslint-disable */
+const fs = require('fs');
+const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
