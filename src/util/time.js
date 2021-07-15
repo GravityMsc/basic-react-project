da949ad7d0d86792f75dc467658572968d454070
GravityMsc <gaotianxiang@cethik.com>
2021-07-15 16:30:35 +0800

    Add

---
+++ src/util/time.js
@@ -0,0 +1,11 @@
+const friendlyTime = (mss) => {
+  const days = parseInt(mss / (1000 * 60 * 60 * 24));
+  const hours = parseInt((mss % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = parseInt((mss % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = (mss % (1000 * 60)) / 1000;
+  return days + " 天 " + hours + " 小时 " + minutes + " 分钟 " + seconds + " 秒 ";
+};
+
+export {
+  friendlyTime
+};
