42b5a52ef946343941725d602a37dcb20460f8d8
GravityMsc <gaotianxiang@cethik.com>
2021-07-16 10:53:15 +0800

    Fix: eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,15 @@
 const eslintrc = {
-  "extends": ["eslint-config-cetitife"],
-  "rules": {
-    "jsx-a11y/anchor-is-valid": ["error", {
-      "components": ["Link", "NavLink"],
-      "specialLink": ["to"]
-    }] // fix react-router component <Link to="/path"> error
-  }
+  extends: ['eslint-config-cetitife'],
+  parserOptions: {
+    babelOptions: {
+      configFile: `${__dirname}/.babelrc.json`,
+    },
+  },
+  rules: {
+    'jsx-a11y/anchor-is-valid': ['error', {
+      components: ['Link', 'NavLink'],
+      specialLink: ['to'],
+    }], // fix react-router component <Link to="/path"> error
+  },
 };
 module.exports = eslintrc;
